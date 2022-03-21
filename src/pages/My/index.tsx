edc085d52fa46aa9965b41f1c175934466239e89
张文美 <8486998+zhang-wenmei@user.noreply.gitee.com>
2022-03-21 22:33:57 +0800

    feat: 基本信息获取

--- src/pages/My/index.tsx
+++ src/pages/My/index.tsx
@@ -1,22 +1,33 @@
 import { View, Text } from '@tarojs/components';
-import Taro from '@tarojs/taro';
+import Taro, { UserInfo } from '@tarojs/taro';
 import { Observer, useObserver } from 'mobx-react';
-import { useEffect } from 'react';
-import { AtButton } from 'taro-ui';
+import { useEffect, useState } from 'react';
+import { AtAvatar, AtButton } from 'taro-ui';
 import { localGlobal } from '@/store/global';
 
 import styles from './index.module.less';
 
 
 const Index: Taro.FC = () => {
+  const [userInfo, setUserInfo] = useState<UserInfo>()
+  
   useEffect(()=>{
+    Taro.getUserInfo({
+      success: function(res) {
+        setUserInfo(res?.userInfo)
 
+        console.log(res);
+        
+      }
+    })
   },[])
 
   return (
     <Observer>
       {() => (
         <View className={styles.container}>
+          {userInfo?.nickName}
+          <AtAvatar circle image={userInfo?.avatarUrl} />
           <AtButton type='primary' onClick={() => localGlobal.setCount()}>
             SSSS
           </AtButton>
