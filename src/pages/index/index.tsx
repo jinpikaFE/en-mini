d9277a3765083d11576cce648a821d3c9c310405
张文美 <8486998+zhang-wenmei@user.noreply.gitee.com>
2022-03-21 15:43:40 +0800

    init

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,23 @@
+import { View, Text } from '@tarojs/components';
+import { AtButton } from 'taro-ui';
+
+import styles from './index.module.less';
+
+const Index = () => {
+  return (
+    <View className={styles.index}>
+      <Text>Hello world!</Text>
+      <AtButton type='primary'>I need Taro UI</AtButton>
+      <Text>Taro UI 支持 Vue 了吗？</Text>
+      <AtButton type='primary' circle>
+        支持
+      </AtButton>
+      <Text>共建？</Text>
+      <AtButton type='secondary' circle>
+        来
+      </AtButton>
+    </View>
+  );
+};
+
+export default Index;
